064b4d529cfa5e2302ccc595a52f0088ccba396a
daquacks <cristian.mitrea.2007@gmail.com>
2026-02-28 09:52:32 -0500

    Initial commit

---
+++ src/main/java/org/example/clubmanager/ClubManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.clubmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagerApplication.class, args);
+	}
+
+}
